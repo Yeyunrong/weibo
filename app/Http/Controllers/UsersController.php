569f7823354eab93e060519fce0bbc90a890c9f5
YeYunRong <3148113255@qq.com>
2022-07-01 21:22:43 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,17 @@ class UsersController extends Controller
     {
         return view('users.show',compact('user'));
     }
+
+    /**
+     * 处理用户表单
+     */
+    public function store($request)
+    {
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
