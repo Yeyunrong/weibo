73c8aea2905a4cc0057a0054c012ff5ed0aece22
YeYunRong <3148113255@qq.com>
2022-07-05 13:55:49 +0800

    社交统计信息

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Seeders;
+namespace  Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
@@ -9,10 +9,12 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        //设置填充数据生成顺序
         Model::unguard();
 
         $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
 
         Model::reguard();
     }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -18,7 +18,7 @@ class UsersTableSeeder extends Seeder
 
         $user = User::find(1);
         $user->name = 'yyr';
-        $user->email = 'yyr@email.qq';
+        $user->email = 'yyr@email.com';
         $user->is_admin = true;
         $user->save();
     }
